5db6abbf58059b28164f34013ca0768556dba85c
Johan <johan.tique@analisistem.com>
2023-02-12 23:06:48 +0000

    step-17

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_224901) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_12_225904) do
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
