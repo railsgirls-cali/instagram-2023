b1ad45cdfd4ba389765a8870567992a03ef05a8e
Johan <johan.tique@analisistem.com>
2023-02-13 01:36:56 +0000

    step-25

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :description, presence: true
+  
+  has_rich_text :description
 
   has_one_attached :image
 
